acfeac5d855df415548b799a6d040adf03396ac0
Kevin Thomas <kevin.thomas@tigerspike.com>
2015-04-11 23:37:04 +0100

    Updated the pitch example again, managed to lose the previously applied fix

--- Exercises/4-Pitch/pitch.rb
+++ Exercises/4-Pitch/pitch.rb
@@ -7,6 +7,7 @@ def pitch_class note
     when note.size == 2 && notes.key?(note[0]) && (note[1] == "#" || note[1] == "b")
       result = notes[note[0]]
       result += (note[1] == "#") ? 1 : -1
+      result = (result < notes["C"]) ? result += notes["B"]+1 : (result > notes["B"]) ? result -= notes["B"]+1 : result
     else
       result
   end
