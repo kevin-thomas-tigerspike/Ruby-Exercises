bacaa9f1128c5ccdd23670e98e0477c89482621c
Kevin Thomas <kevin.thomas@tigerspike.com>
2015-04-11 19:57:28 +0100

    Updated tests to remove requirements for invalid inputs check, and refactored

--- Exercises/5-GridBlast/grid_blast.rb
+++ Exercises/5-GridBlast/grid_blast.rb
@@ -1,9 +1,7 @@
+$grid = ['top left',    'top middle',    'top right',
+         'middle left', 'center',        'middle right',
+         'bottom left', 'bottom middle', 'bottom right']
+
 def fire(x,y)
-  grid = ['top left',    'top middle',    'top right',
-          'middle left', 'center',        'middle right',
-          'bottom left', 'bottom middle', 'bottom right']
-  location = [[0,1,2],[0,3,6]]
-  return nil unless (x.is_a? Integer) && (y.is_a? Integer) # error if not numbers
-  return nil unless ((0..2).cover? x) && ((0..2).cover? y) # error if not between correct range
-  grid[location[0][x.to_i]+location[1][y.to_i]]
+  $grid[x + y*3]
 end
--- Tests/grid_blast_spec.rb
+++ Tests/grid_blast_spec.rb
@@ -1,13 +1,6 @@
 require_relative '../Exercises/5-GridBlast/grid_blast'
 
 describe 'Return the grid location name' do
-  it 'should return nil for invalid input' do
-    expect(fire("a",0)).to equal nil
-    expect(fire(0,"b")).to equal nil
-    expect(fire(-1,1)).to equal nil
-    expect(fire(1,-1)).to equal nil
-  end
-
   it 'should return the correct grid location name' do
     expect(fire(0,0)).to eq("top left")
     expect(fire(1,0)).to eq("top middle")
